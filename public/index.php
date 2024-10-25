5f2d11bf3ebacb440ea0213f5a9501423fea6e86
thalis <thalis@ipeweb.com.br>
2024-10-25 18:06:04 -0300

    [fix]

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php 
+header('Acess-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+require_once '../vendor/autoload.php';
+$connPdo = new \PDO(DBDRIVE . ':host=' . DBHOST . ';dbname=' . DBNAME, DBUSER, DBPASS);
+
+var_dump($connPdo);
+
+if ($_GET['url']) {
+    $url = explode('/', $_GET['url']);
+}
--- src/Router.php
+++ src/Router.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Router
 {
     public function getRouter($method, $rout): array
