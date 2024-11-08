0f8d7373c6570072dd70b79420f4517f8f281e1e
thalis <thalis@ipeweb.com.br>
2024-11-08 17:59:49 -0300

    fix

--- src/Models/Database.php
+++ src/Models/Database.php
@@ -7,14 +7,15 @@ use PDO;
 class Database
 {
     const DBDRIVE = 'pgsql';
-    const DBHOST = 'postgres';
+    const DBHOST = 'localhost';
     const DBPORT = '5432';
     const DBNAME = 'spotifaux';
     const DBUSER = 'pericao';
     const DBPASS = '123';
+
     public static function getConnection()
     {
-        $pdo = new PDO("pgsql:host=localhost;port=5432;dbname=spotifaux", "pericao", "123");
-        return $pdo;
+        $connPdo = new PDO("pgsql:host=postgres_db;port=5432;dbname=spotifaux", "pericao", "123");
+        return $connPdo;
     }
 }
